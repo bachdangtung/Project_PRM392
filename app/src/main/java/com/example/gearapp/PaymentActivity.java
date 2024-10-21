175cbded71e55c8846c75d690406a3e012dfb826
thai0502 <thaidqhe170966@fpt.edu.vn>
2024-10-21 08:11:21 +0700

    Change Name "Thanh Toan" to "Payment"

--- app/src/main/java/com/example/gearapp/PaymentActivity.java
+++ app/src/main/java/com/example/gearapp/PaymentActivity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ThanhToanActivity extends AppCompatActivity {
+public class PaymentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_thanh_toan);
+        setContentView(R.layout.activity_payment);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
