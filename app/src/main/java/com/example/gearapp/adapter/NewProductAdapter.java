3b7f4dda9206ce7932a85160cf655b28e20b9b1a
CuongTruong93 <134049101+CuongTruong93@users.noreply.github.com>
2024-11-04 01:21:35 +0700

    Cart

--- app/src/main/java/com/example/gearapp/adapter/NewProductAdapter.java
+++ app/src/main/java/com/example/gearapp/adapter/NewProductAdapter.java
@@ -1,6 +1,7 @@
 package com.example.gearapp.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.gearapp.R;
+import com.example.gearapp.activity.CartActivity;
 import com.example.gearapp.model.NewProduct;
 
 import java.text.DecimalFormat;
@@ -19,10 +21,10 @@ import java.util.List;
 
 public class NewProductAdapter extends RecyclerView.Adapter<NewProductAdapter.MyViewHolder> {
     Context context;
-    List<NewProduct> array;
+    List<NewProduct> productList;
 
-    public NewProductAdapter(List<NewProduct> array, Context context) {
-        this.array = array;
+    public NewProductAdapter(List<NewProduct> productList, Context context) {
+        this.productList = productList;
         this.context = context;
     }
 
@@ -30,36 +32,45 @@ public class NewProductAdapter extends RecyclerView.Adapter<NewProductAdapter.My
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product_new, parent, false);
-
         return new MyViewHolder(item);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        NewProduct newProduct = array.get(position);
-        holder.txtname.setText(newProduct.getName());
+        NewProduct product = productList.get(position);
+        holder.txtName.setText(product.getName());
+
+        // Format price as a currency
         DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
-        //holder.txtprice.setText("Price: "+decimalFormat.format(Double.parseDouble(newProduct.getPrice()))+"Đ");
-        holder.txtprice.setText(newProduct.getPrice()+ "");
-        Glide.with(context).load(newProduct.getImage()).into(holder.imgimage);
+        holder.txtPrice.setText("Price: " + decimalFormat.format(Double.parseDouble(product.getPrice())) + "Đ");
+
+        // Load image using Glide
+        Glide.with(context).load(product.getImage()).into(holder.imgImage);
+
+        // Set click listener for each item
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, CartActivity.class);
+            intent.putExtra("productName", product.getName());
+            intent.putExtra("productPrice", product.getPrice());
+            intent.putExtra("productImage", product.getImage());
+            context.startActivity(intent);
+        });
     }
 
     @Override
     public int getItemCount() {
-        return array.size();
+        return productList.size();
     }
 
-
-    public class MyViewHolder extends  RecyclerView.ViewHolder{
-        TextView txtprice, txtname;
-        ImageView imgimage;
-
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView txtPrice, txtName;
+        ImageView imgImage;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            txtprice = itemView.findViewById(R.id.itemproduct_price);
-            txtname = itemView.findViewById(R.id.itemproduct_name);
-            imgimage = itemView.findViewById(R.id.itemproduct_image);
+            txtPrice = itemView.findViewById(R.id.itemproduct_price);
+            txtName = itemView.findViewById(R.id.itemproduct_name);
+            imgImage = itemView.findViewById(R.id.itemproduct_image);
         }
     }
 }
