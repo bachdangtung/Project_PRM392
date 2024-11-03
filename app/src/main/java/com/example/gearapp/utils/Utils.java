b38afc87cf3d836369994d00577ff786f6afc28f
NGUYEN NGOC QUANG <quangnnhe171324@fpt.edu.vn>
2024-11-04 00:40:38 +0700

    xóa class API, dùng sqlite

--- app/src/main/java/com/example/gearapp/utils/Utils.java
+++
@@ -1,7 +0,0 @@
-package com.example.gearapp.utils;
-
-public class Utils {
-    //public static  final String BASE_URL="http://192.168.1.18/selling/";
-    public static  final String BASE_URL="http://10.0.2.2/selling/";
-
-}
